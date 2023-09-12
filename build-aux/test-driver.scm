0d73591b0c24254447eccab8efc76317d3c2618e
Ricardo Wurmus <rekado@elephly.net>
2023-09-12 15:22:36 +0200

    REUSE 3.0 compliance.

--- build-aux/test-driver.scm
+++ build-aux/test-driver.scm
@@ -17,6 +17,8 @@
 ;;; You should have received a copy of the GNU General Public License
 ;;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+;;; SPDX-License-Identifier: GPL-3.0-or-later
+
 ;;;; Commentary:
 ;;;
 ;;; This script provides a Guile test driver using the SRFI-64 Scheme API for
--- etc/shepherd-config.scm
+++ etc/shepherd-config.scm
@@ -1,3 +1,7 @@
+;; SPDX-FileCopyrightText: 2023 Ricardo Wurmus <rekado@elephly.net>
+;;
+;; SPDX-License-Identifier: CC0-1.0
+
 (use-modules (swineherd)
              (shepherd service))
 (register-services (list swineherd-service
